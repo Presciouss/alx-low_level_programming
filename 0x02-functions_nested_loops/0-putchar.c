e2737a24eb3157b49a57713b3e71aa2be0b3d60b
Presciouss <presciouss.mothapo@gmail.com>
2023-07-19 05:53:50 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main -Prints "_putchar" folowed by a new line
+ * main - Prints "_putchar" followed by a new line.
  *
- * Reyurn: always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
-	-putchar
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
+	return (0);
+}
