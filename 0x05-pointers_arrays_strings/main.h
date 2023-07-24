1b0604d21446c4d026c8b684d000d4295b6ae41d
Preciouss <presciouss.mothapo@gmail.com>
2023-07-24 11:53:06 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+void reset_to_98(int *n);
+int _strlen(char *s);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void puts2(char *str);
+char *_strcpy(char *dest, char *src);
+void swap_int(int *a, int *b);
+void print_array(int *a, int n);
+
