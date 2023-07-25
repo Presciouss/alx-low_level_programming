b15d148d096957cf21caa272a83be5b532228ace
Preciouss <presciouss.mothapo@gmail.com>
2023-07-25 14:10:16 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,17 +1,10 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-void print_to_98(int n);
-void print_times_table(int n);
+#include <stdio.h>
 
+void positive_or_negative(int i);
+int largest_number(int, int, int );
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 #endif
