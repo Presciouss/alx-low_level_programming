529968f3d3f62ec10f763917079983eda7864c50
Preciouss <presciouss.mothapo@gmail.com>
2023-07-25 04:46:01 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * puts2 - a function that prints every other character of a string,
- *  starting with the first character, followed by a new line.
- *
- *  @s: pointer string
- *
- *
+ * puts2 - a function that prints every other character of a string, 
+ *         starting with the first character, followed by a new line.
+ *                  
+ *  @s: pointer to a string
+ * 
+ * 
  */
 
 void puts2(char *s)
