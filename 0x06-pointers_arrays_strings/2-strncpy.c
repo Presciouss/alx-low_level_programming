3208d470ff896b1ff2276f0de052eb943cedff78
Presciouss <presciouss.mothapo@gmail.com>
2023-07-26 13:40:01 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * _strncpy - function that copies a string.
- * @dest : Input
- * @src : Input
+ * @dest: Input
+ * @src: Input
  * @n: Input
  *
- * Return : dest
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 
