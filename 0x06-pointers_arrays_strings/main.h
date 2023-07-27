a8a12c21c261c83d99fa1d5808c719b0743cced9
Presciouss <presciouss.mothapo@gmail.com>
2023-07-27 05:11:17 -0700

    1-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,4 +7,5 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-int _putchar(char);               
+int _putchar(char);              
+char *_strcat(char *dest, char *src);
