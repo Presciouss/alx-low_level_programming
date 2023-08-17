495d65d155c0579264b400354880eceea54cf594
Presciouss <presciouss.mothapo@gmail.com>
2023-08-17 04:54:46 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ *  print_name - function that prints a name.
+ *  @name: added string
+ *  @f: pointr
+ *  Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	return;
+
+	f(name);
+}
